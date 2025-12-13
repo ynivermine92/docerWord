365fd377ec8fa61b94d3c4bb7186018b649401c3
ynivemrine92 <ynivermine92@gmail.com  git config --global user.name ynivemrine92git commit -m 'v1'$  git config --global user.email ynivermine92@gmail.com>
2025-12-13 15:39:30 +0200

    v1

---
+++ main/wordpress/wp-content/themes/theme-main/page-constructor.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ * Template Name: page constructor
+ */
+get_header();
+?>
+
+
+
+
+
+<section class="section portfolio">
+    <div class="container">
+        <div class="row">
+            <h2 class="title fw700 ts-32">Portfolio</h2>
+        </div>
+
+
+
+
+
+      
+
+    </div>
+</section>
+
+
+
+<script src='/assets/js/main.js'></script>
+
+
+
+
+<?php get_footer(); ?>
